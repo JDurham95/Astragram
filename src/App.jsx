bcc331fdb0763ff32f66862463266df28f687aee
JDurham95 <jacobgdurham@gmail.com>
2025-03-24 13:36:13 -0600

    Dog head routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,27 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css'
 import Navigation from './components/Navigation'
 import HomePage from './pages/HomePage'
-import dogHead from "/Astra_vector.svg"
+
 
 
 function App() {
 
+  const backgroundStyle = {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100vw",
+    height: "100vh",
+    backgroundImage: "url(/dog_head.png)",
+    backgroundSize: "150px 150px",
+    backgroundRepeat: "repeat",
+    animation: "panBackground 15s linear infinite",
+    
+  };
+
   return (
     <div className='app'>
-      <div className='background'>
+      <div style={backgroundStyle}>
       <header>
         <h1 className='astragram-home-header'>ASTRAGRAM</h1>
       </header>
